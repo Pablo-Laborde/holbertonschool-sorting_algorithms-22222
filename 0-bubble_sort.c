b6df42e06b4112487485a055b10119056d883bc8
Pablo <p5.tacoronte@gmail.com>
2023-05-17 10:06:07 -0700

    fix > instead of < and betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,23 +1,29 @@
 #include "sort.h"
 
+/**
+ * bubble_sort- sorts
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, aux;
-    int flag;
+		size_t i, aux;
+		int flag;
 
-    do
-    {
-        flag = 0;
-        for (i = 0; i < (size - 1); i++)
-        {
-            if (array[i] < array[i + 1])
-            {
-                aux = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = aux;
-                flag = 1;
-            }
-        }
-        print_array(array, size);
-    } while (flag);
+		do
+		{
+				flag = 0;
+				for (i = 0; i < (size - 1); i++)
+				{
+						if (array[i] > array[i + 1])
+						{
+								aux = array[i];
+								array[i] = array[i + 1];
+								array[i + 1] = aux;
+								flag = 1;
+						}
+				}
+				print_array(array, size);
+		} while (flag);
 }
