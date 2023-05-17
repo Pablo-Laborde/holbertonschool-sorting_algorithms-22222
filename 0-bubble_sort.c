9f14e54c05d19273f97aae0df42db705a5375f49
Pablo <p5.tacoronte@gmail.com>
2023-05-17 10:02:08 -0700

    exercise 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, aux;
+    int flag;
+
+    do
+    {
+        flag = 0;
+        for (i = 0; i < (size - 1); i++)
+        {
+            if (array[i] < array[i + 1])
+            {
+                aux = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = aux;
+                flag = 1;
+            }
+        }
+        print_array(array, size);
+    } while (flag);
+}
