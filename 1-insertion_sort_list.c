1d3949837d71e178363b64f6015640cb06eae519
Pablo <p5.tacoronte@gmail.com>
2023-05-17 21:36:03 -0700

    change

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,8 +4,33 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *a = NULL, *b = NULL, *c = NULL;
 
-	if (list && *list)
+	if (list && *list && (*list)->next)
 	{
 		a = *list;
+		b = a->next;
+		do {
+		} while (flag);
 	}
 }
+
+/**
+ * change- changes two elemnts of a list
+ * @a: first element
+ * @b: second element
+ * Return: void
+ */
+ void change(listint_t *a, listint_t *b)
+ {
+	listint_t *n = NULL, *p = NULL;
+
+	n = b->next;
+	p = a->prev;
+	b->next = a;
+	b->prev = p;
+	a->next = n;
+	a->prev = b;
+	if (p)
+		p->next = b;
+	if (n)
+		n->prev = a;
+ }
