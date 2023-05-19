40aad21e7bc326a3f9653e343150233537fb5288
Pablo <p5.tacoronte@gmail.com>
2023-05-18 22:22:15 -0700

    fixes

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,7 +7,7 @@
  */
 void quick_sort(int *array, size_t size)
 {
-  sort_rev(array, size, 0, (size - 1));
+	sort_rev(array, size, 0, (size - 1));
 }
 
 /**
@@ -20,32 +20,35 @@ void quick_sort(int *array, size_t size)
  */
 void sort_rev(int *array, size_t size, size_t min, size_t max)
 {
-  int a;
-  size_t x, i, j;
+	int a;
+	size_t x, i, j;
 
-  if (min != max)
-  {
-    i = min;
-    j = max;
-    while (i != j)
-    {
-      if (array[j] >= array[i])
-        if (j > i)
-          j--;
-        else
-          j++;
-      else
-      {
-        a = array[j];
-        array[j] = array[i];
-        array[i] = a;
-        x = j;
-        j = i;
-        i = x;
-        print_array(array, size);
-      }
-    }
-    sort_rev(array, size, 0, (i - 1));
-    sort_rev(array, size, (i + 1), max);
-  }
+	if (min != max)
+	{
+		i = min;
+		j = max;
+		while (i != j)
+		{
+			if (array[j] >= array[i])
+				if (j > i)
+					j--;
+				else
+					j++;
+			else
+			{
+				a = array[j];
+				array[j] = array[i];
+				array[i] = a;
+				x = j;
+				j = i;
+				i = x;
+				print_array(array, size);
+			}
+		}
+		if (i > min && i < max)
+		{
+			sort_rev(array, size, min, (i - 1));
+			sort_rev(array, size, (i + 1), max);
+		}
+	}
 }
